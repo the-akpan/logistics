81474c2373fbdaec06d5151693cf6230d38b37dc
Daniel Akpan <akpandaniel.n@gmail.com>
2023-02-08 18:30:48 +0100

    link react to express

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "path";
 
 const app = express();
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/logistics";
 const PORT = process.env.PORT || 3000;
+
+const __dirname = new URL(".", import.meta.url).pathname;
 
 mongoose.set("strictQuery", false);
 mongoose
@@ -14,6 +17,12 @@ mongoose
     console.log(`${time.toISOString()}: MongoDB connected`);
   })
   .catch((err) => console.error(err));
+
+// Register react
+app.use(express.static("client/dist"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("Running on port " + PORT);
